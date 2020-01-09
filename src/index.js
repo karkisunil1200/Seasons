4a7e1722eb17ae476f291d036b62a1cbdfcdc7a6
Sunil Karki <karkisunil1200@gmail.com>
2020-01-09 12:34:46 -0600

    Getting the lattitude

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return (
-    <div>
-      <h1>HELLO</h1>
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      lat: null
+    };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({lat: position.coords.latitude});
+      },
+      err => console.log(err)
+    );
+  }
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
